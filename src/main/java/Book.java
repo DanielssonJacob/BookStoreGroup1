d33e4782b8d30fc193ffb5416fb369773d1e0e40
eddestlamd <amanda.eddestal@hotmail.com>
2022-02-16 14:06:37 +0100

    Commit to book

---
+++ src/main/java/Book.java
@@ -0,0 +1,54 @@
+public class Book {
+
+    int bookId;
+    String title;
+    String author;
+    double price;
+
+    public Book() {
+    }
+
+    public Book(int bookId, String title, String author, double price) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String bookToString(){
+        return "BookId: " + this.bookId + " || Title: " + this.title + "|| Author: " + this.author + "|| Price: " + this.price;
+    }
+
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
